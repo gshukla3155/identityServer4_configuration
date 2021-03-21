d2d03aabaa71bb6d797496e4c8f7cbda05e5af96
Ghanshyam Shukla <70849493+gshukla3155@users.noreply.github.com>
2021-03-21 22:46:13 +0530

    Add files via upload

---
+++ IdentityServer4_Configuration/Helper/TestUsers.cs
@@ -0,0 +1,48 @@
+﻿using IdentityModel;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace IdentityServer4_Configuration.Helper
+{
+    public class TestUsers
+    {
+        public static List<TestUser> Users
+        {
+            get
+            {                                
+                return new List<TestUser>
+                {
+                    new TestUser
+                    {
+                        SubjectId = "1",
+                        Username = "sample@admin.com",
+                        Password = "secret",
+                        Claims =
+                        {
+                            new Claim(JwtClaimTypes.Name, "Dev User"),
+                            new Claim(JwtClaimTypes.GivenName, "Dev"),
+                            new Claim(JwtClaimTypes.FamilyName, "User"),
+                            new Claim(JwtClaimTypes.Email, "sample@admin.com"),
+                            new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),                            
+                        }
+                    },
+                    new TestUser
+                    {
+                        SubjectId = "2",
+                        Username = "test@admin.com",
+                        Password = "secret",
+                        Claims =
+                        {
+                            new Claim(JwtClaimTypes.Name, "Test User"),
+                            new Claim(JwtClaimTypes.GivenName, "Test"),
+                            new Claim(JwtClaimTypes.FamilyName, "User"),
+                            new Claim(JwtClaimTypes.Email, "test@admin.com"),
+                            new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        }
+                    }
+                };
+            }
+        }
+    }
+}
